d0b513ab195cfcf7abc39af641c385c3abeb727a
dahmoulahmedtaha <dahmoulahmedtaha@gmail.com>
2025-12-20 11:43:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ class MemoryOptimizedInstantMesh:
         print("Loading Diffusion Pipeline...")
         self.pipeline = DiffusionPipeline.from_pretrained(
             "sudo-ai/zero123plus-v1.2",
-            custom_pipeline="sudo-ai/zero123plus-v1.2",
+            custom_pipeline="sudo-ai/zero123plus-pipeline",
             torch_dtype=torch.float16,
         )
         self.pipeline.scheduler = EulerAncestralDiscreteScheduler.from_config(
